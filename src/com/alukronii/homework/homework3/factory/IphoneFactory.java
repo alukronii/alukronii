0a988fb0c27cac70f638d878e21e598a0d00417f
alukronii <ssokiran@yahoo.com>
2024-08-14 23:58:15 +0200

    Готово

--- src/com/alukronii/homework/homework3/factory/IphoneFactory.java
+++ src/com/alukronii/homework/homework3/factory/IphoneFactory.java
@@ -1,13 +1,20 @@
 package com.alukronii.homework.homework3.factory;
 
+import com.alukronii.homework.homework3.factory.phone.Body;
+import com.alukronii.homework.homework3.factory.phone.Camera;
+import com.alukronii.homework.homework3.factory.phone.Iphone;
+import com.alukronii.homework.homework3.factory.phone.Processor;
+
 public class IphoneFactory {
 
-    Processor processor1 = new Processor(1500);
-    Processor processor2 = new Processor(1500);
-    Camera camera = new Camera(8);
-    Body body = new Body(60, 200, 10);
+    private static Processor processor1 = new Processor(1500);
+    private static Processor processor2 = new Processor(1500);
+    private static Camera camera = new Camera(8);
+    private static Body body = new Body(60, 200, 10);
 
-    Iphone iphone = new Iphone(processor1, processor2, camera, body);
-
-
+    public static Iphone createSamsung () {
+        Iphone iphone = new Iphone(processor1, processor2, camera, body);
+        System.out.println("Телефон Iphone создан");
+        return iphone;
+    }
 }
