65b3adb5c74eada5d5bc2098025fcbd14c7f30ad
alukronii <ssokiran@yahoo.com>
2024-08-15 07:30:08 +0200

    Готово

--- src/com/alukronii/homework/homework3/city/City.java
+++ src/com/alukronii/homework/homework3/city/City.java
@@ -2,9 +2,23 @@ package com.alukronii.homework.homework3.city;
 
 import java.util.Arrays;
 
-public final class City {
-    private final String cityName;
-    private final House[] houses;
+public class City implements Cloneable{
+    private String cityName;
+    private House[] houses;
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public City(String cityName, House[] houses) {
+        this.cityName = cityName;
+        this.houses = houses;
+    }
+
+    public City (City newCityCopy) throws CloneNotSupportedException {
+        this(newCityCopy.getCityName(), newCityCopy.getHouses().clone());
+    }
 
     public String getCityName() {
         return cityName;
@@ -14,17 +28,11 @@ public final class City {
         return houses;
     }
 
-    public City(String cityName, House[] houses) {
-        this.cityName = cityName;
-        this.houses = houses;
-
-    }
-
     @Override
     public String toString() {
         return "City{" +
                 "cityName='" + cityName + '\'' +
-                ", houses=" + Arrays.toString(houses.clone()) +
+                ", houses=" + Arrays.toString(houses) +
                 '}';
     }
 }
